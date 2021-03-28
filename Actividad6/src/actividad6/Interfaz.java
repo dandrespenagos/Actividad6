422f0db1961a9a3488a734a77e1934c016800691
Andres <80366857+dandrespenagos@users.noreply.github.com>
2021-03-28 11:28:25 -0500

    Se modifico la interfaz principal

--- Actividad6/src/actividad6/Interfaz.java
+++ Actividad6/src/actividad6/Interfaz.java
@@ -1,6 +1,8 @@
 
 package actividad6;
 
+import javax.swing.JOptionPane;
+
 
 public class Interfaz extends javax.swing.JFrame {
 
@@ -15,65 +17,78 @@ public class Interfaz extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        paciente = new javax.swing.JButton();
-        medico = new javax.swing.JButton();
-        centrodeservicio = new javax.swing.JButton();
+        consultar = new javax.swing.JButton();
+        agregar = new javax.swing.JButton();
+        modificar = new javax.swing.JButton();
+        retirar = new javax.swing.JButton();
+        salir = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(204, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("CONSULTA MEDICA");
-        jLabel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jLabel1.setText("INFORMACION DE PACIENTES");
 
-        paciente.setBackground(new java.awt.Color(255, 255, 255));
-        paciente.setText("CONSULTA POR PACIENTE");
-        paciente.addActionListener(new java.awt.event.ActionListener() {
+        consultar.setText("Consultar paciente");
+
+        agregar.setText("Agregar Paciente");
+
+        modificar.setText("Modificar informacion del paciente");
+
+        retirar.setText("Retirar paciente");
+
+        salir.setText("Salir");
+        salir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pacienteActionPerformed(evt);
+                salirActionPerformed(evt);
             }
         });
-
-        medico.setBackground(new java.awt.Color(255, 255, 255));
-        medico.setText("CONSULTA POR MEDICO");
-
-        centrodeservicio.setBackground(new java.awt.Color(255, 255, 255));
-        centrodeservicio.setText("CONSULTA POR CENTRO DE SERVICIO");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(97, 97, 97)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(medico)
-                    .addComponent(paciente))
-                .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(129, 129, 129)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(agregar)
+                            .addComponent(consultar)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(21, 21, 21)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(68, 68, 68)
-                        .addComponent(centrodeservicio)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(salir)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(modificar)
+                        .addGap(70, 70, 70))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(retirar)
+                        .addGap(110, 110, 110))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(72, 72, 72)
-                .addComponent(paciente)
-                .addGap(34, 34, 34)
-                .addComponent(medico)
-                .addGap(27, 27, 27)
-                .addComponent(centrodeservicio)
-                .addContainerGap(65, Short.MAX_VALUE))
+                .addGap(31, 31, 31)
+                .addComponent(consultar)
+                .addGap(18, 18, 18)
+                .addComponent(agregar)
+                .addGap(18, 18, 18)
+                .addComponent(modificar)
+                .addGap(18, 18, 18)
+                .addComponent(retirar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addComponent(salir)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -92,11 +107,14 @@ public class Interfaz extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void pacienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pacienteActionPerformed
+    private void salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salirActionPerformed
+        
+        
+        JOptionPane.showMessageDialog(this, "Gracias por usar nuestro servicio, buen dia");
         
         
         
-    }//GEN-LAST:event_pacienteActionPerformed
+    }//GEN-LAST:event_salirActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -131,10 +149,12 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton centrodeservicio;
+    private javax.swing.JButton agregar;
+    private javax.swing.JButton consultar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton medico;
-    private javax.swing.JButton paciente;
+    private javax.swing.JButton modificar;
+    private javax.swing.JButton retirar;
+    private javax.swing.JButton salir;
     // End of variables declaration//GEN-END:variables
 }
